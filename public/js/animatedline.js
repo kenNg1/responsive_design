51aeb7da832c7fecc90bb4d8d27233fc90bd6bc1
kenNg1 <kng1230@hotmail.com>
2017-05-17 11:10:08 +0800

    implement responsive for svg animation, however multiple scenes created revisit

--- public/js/animatedline.js
+++ public/js/animatedline.js
@@ -4,6 +4,9 @@ function pathPrepare ($el) {
 	var lineLength = $el[0].getTotalLength();
 	$el.css("stroke-dasharray", lineLength);
 	$el.css("stroke-dashoffset", lineLength);
+	ts_o_height = 1174;
+	ts_n_height = ($('div.ts').height());
+	factor = ts_n_height/ts_o_height;
 }
 
 var $word = $("path#word");
@@ -23,13 +26,34 @@ var tween = new TimelineMax()
 	.add(TweenMax.to("path", 1, {stroke: "rgb(235, 255, 0)", ease:Linear.easeNone}), 0);			// change color during the whole thing
 
 //build scene
-var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 1300, offset: -310, tweenChanges: false})
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: ts_o_height * factor, offset: 0, tweenChanges: false})
 				.setTween(tween)
-				.addIndicators() // add indicators (requires plugin)
+				// .addIndicators() // add indicators (requires plugin)
 				.addTo(controller);
 
 scene.on("start", function (event) {
     console.log("Hit start point of scene.");
 });
 
-$("#word").attr("width",)
+window.onresize = function(){
+	pathPrepare($word);
+	pathPrepare($dot);
+	// init controller
+	var controller = new ScrollMagic.Controller();
+
+	// build tween
+	var tween = new TimelineMax()
+		.add(TweenMax.to($word, 1, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
+		.add(TweenMax.to($dot, 0.1, {strokeDashoffset: 0, ease:Linear.easeNone}))  // draw dot for 0.1
+		.add(TweenMax.to("path", 1, {stroke: "rgb(235, 255, 0)", ease:Linear.easeNone}), 0);			// change color during the whole thing
+
+	//build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: ts_o_height * factor, offset: 0, tweenChanges: false})
+					.setTween(tween)
+					// .addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
+
+	scene.on("start", function (event) {
+	    console.log("Hit start point of scene.");
+	});
+};
